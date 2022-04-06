dc0bf61dc450993a09defeebf8aff1ff0a30c0a8
yungtimofey <super.tim-stepanov@yandex.ru>
2022-04-06 20:59:25 +0300

    try to init my branch p.2

---
+++ src/main/java/com/debugteam/auction_test/models/Bet.java
@@ -0,0 +1,13 @@
+package com.debugteam.auction_test.models;
+
+public class Bet {
+//    public Bet()
+//    {
+//
+//    }
+    private User owner;
+    //private Lot lot; //i don`t have Lot class
+    private Integer id;
+    private Integer betSize;
+    private java.time.Clock betTime; //When was bet made.
+}
---
+++ src/main/java/com/debugteam/auction_test/models/LotRequest.java
@@ -0,0 +1,10 @@
+package com.debugteam.auction_test.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class LotRequest {
+    String name;
+    int startPrice;
+    LocalDateTime timeExistence;
+}
