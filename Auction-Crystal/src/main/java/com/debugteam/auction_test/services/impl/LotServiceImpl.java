9f3dd772dde016cb2222dded08b6822244a17123
Pavel <kulikov-pavlik@bk.ru>
2022-04-13 22:17:53 +0300

    resolved problems after merge

--- Auction-Crystal/src/main/java/com/debugteam/auction_test/services/impl/LotServiceImpl.java
+++ Auction-Crystal/src/main/java/com/debugteam/auction_test/services/impl/LotServiceImpl.java
@@ -2,9 +2,11 @@ package com.debugteam.auction_test.services.impl;
 
 import com.debugteam.auction_test.database.entities.AccountEntity;
 import com.debugteam.auction_test.database.entities.LotEntity;
+import com.debugteam.auction_test.database.entities.ProductEntity;
 import com.debugteam.auction_test.database.repositories.AccountRepository;
 import com.debugteam.auction_test.database.repositories.LotRepository;
-import com.debugteam.auction_test.exceptions.LotExistsException;
+import com.debugteam.auction_test.database.repositories.ProductRepository;
+import com.debugteam.auction_test.exceptions.*;
 import com.debugteam.auction_test.models.LotDto;
 import com.debugteam.auction_test.models.LotRequest;
 import com.debugteam.auction_test.services.LotService;
@@ -13,49 +15,92 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LotServiceImpl implements LotService {
 
     private final LotRepository lotRepository;
     private final AccountRepository accountRepository;
+    private final ProductRepository productRepository;
     private final ModelMapper mapper;
 
-    public LotServiceImpl(LotRepository lotRepository, AccountRepository accountRepository, ModelMapper mapper) {
+    public LotServiceImpl(LotRepository lotRepository, AccountRepository accountRepository, ProductRepository productRepository, ModelMapper mapper) {
         this.lotRepository = lotRepository;
         this.accountRepository = accountRepository;
+        this.productRepository = productRepository;
         this.mapper = mapper;
     }
 
+
     @Override
-    public List<LotDto> getSearchLots(String name)
-    {
-        return new ArrayList<LotDto>();
+    public List<LotDto> getSearchLots(String name) throws LotNotExistsException {
+        List<LotEntity> foundEntities = lotRepository.findByNameContainingIgnoreCaseOrderByName(name);
+//        if(name == null || (foundEntities.size() == 0)){
+//            throw new LotNotExistsException();
+//        }
+        List<LotDto> listLots = new ArrayList<LotDto>();
+
+        for (LotEntity iter : foundEntities) {
+            listLots.add(mapper.map(iter, LotDto.class));
+        }
+        return listLots;
     }
 
     @Override
-    public LotDto addLot(LotRequest lotRequest, String user_id) throws LotExistsException
-    {
-        // надо ли проверять остальные параметры? И нужны ли тут параметры?
+    public List<LotDto> getLots() {
+        List<LotEntity> lotsEntity = lotRepository.findAll();
 
-        // Проверка именно такая?
-        if (lotRequest.getName() != null && lotRepository.existsByName(lotRequest.getName())) {
+        List<LotDto> listLots = new ArrayList<LotDto>();
+
+        for (LotEntity iter : lotsEntity) {
+            listLots.add(mapper.map(iter, LotDto.class));
+        }
+        return listLots;
+    }
+
+    //Надо добавить productId, но у нас много продуктов, поэтому нужно передавать как-то лист продуктов.
+    @Override
+    public LotDto addLot(LotRequest lotRequest, String userId) throws LotExistsException, AccountNotExistsException
+            , ProductAlreadyInLotException {
+        if (lotRequest.getId() == null || lotRepository.existsById(lotRequest.getId())) {
             throw new LotExistsException();
         }
+        LotEntity lot = mapper.map(lotRequest, LotEntity.class);
 
-        // тут не берется почему-то
-        AccountEntity accountEntity = accountRepository.getById(user_id);
+        Optional<AccountEntity> existedUser = accountRepository.findOptionalById(userId);
+        AccountEntity userEntity = existedUser.orElseThrow(AccountNotExistsException::new);
 
-        LotEntity newLot = mapper.map(lotRequest, LotEntity.class);
-        newLot.setUser(accountEntity);
-        lotRepository.save(newLot);
+        List<ProductEntity> productsEntity = new ArrayList<>();
+        for (String productId : lotRequest.getProductsId()) {
+            ProductEntity productEntity = productRepository.getById(productId);
+            if (productEntity.getLot() != null) {
+                throw new ProductAlreadyInLotException();
+            }
 
-        return mapper.map(newLot, LotDto.class);
+            productEntity.setLot(lot);
+            productsEntity.add(productEntity);
+        }
+        lot.setLotProducts(productsEntity);
+        lot.setUser(userEntity);
+        //TODO: Проход в цикле по списку строк-id товаров из lotRequest.
+        //Здесь все то же самое, что у аккаунта и лота. То есть у нас здесь много лотов и один аккаунт. А нам нужно
+        //сделать много продуктов и один лот.
+
+
+        lotRepository.save(lot);
+
+        return mapper.map(lot, LotDto.class);
     }
 
     @Override
-    public void deleteLot(String lotId)
-    {
-
+    public void deleteLot(String lotId, String userId) throws LotNotExistsException, UserAccessViolationException {
+        Optional<LotEntity> existedLot = lotRepository.findOptionalById(lotId);
+        LotEntity lot = existedLot.orElseThrow(LotNotExistsException::new);
+        AccountEntity accountEntity = accountRepository.getById(userId);
+        if (lot.getUser() != accountEntity) {
+            throw new UserAccessViolationException();
+        }
+        lotRepository.delete(lot);
     }
 }
